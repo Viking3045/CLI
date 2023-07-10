3564e848a5b140a83b54795c3b107df395d3edf3
Vitaliy_Stepanenko <utihar30@gmail.com>
2023-07-10 21:41:36 +0300

    complete dz

---
+++ README.md
@@ -0,0 +1,4 @@
+https://ibb.co/kB2bQrY
+https://ibb.co/CvjZfdg
+https://ibb.co/YfV6vMP
+https://ibb.co/nQGf9V5
--- index.js
+++ index.js
@@ -1,1 +1,33 @@
-console.log('Welcome to Node.js')
+const contacts  = require("./contacts");
+const yargs = require("yargs")
+const { hideBin } = require("yargs/helpers");
+
+const invokeAction = async ({ action, id, name, email, phone }) => {
+  switch (action) {
+    case "list":
+          const listContacts = await contacts.listContacts();
+          return console.log(listContacts);
+   
+
+    case "get":
+       const getContactById = await contacts.getContactById(id);
+          return console.log(getContactById);
+     
+
+      case 'add':
+          const newContacts = await contacts.addContact({ name, email, phone });
+          return console.log(newContacts);
+
+      case 'remove':
+          const removeContacts = await contacts.removeContact(id);
+          return console.log(removeContacts);
+    default:
+      return  console.warn('\x1B[31m Unknown action type!');
+    // default:
+    //   console.warn('\x1B[31m Unknown action type!');
+  }
+}
+
+const arr = hideBin(process.argv);
+const { argv } = yargs(arr);
+invokeAction(argv);
